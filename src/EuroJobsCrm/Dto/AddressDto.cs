1c83520b15934a681574b95e4a489b9c3c4f2e1d
Ivan Syrotiuk <ivan.syrotiuk@gmail.com>
2016-11-01 15:21:16 +0100

    Updated data model and implemented BL for conragents

---
+++ src/EuroJobsCrm/Dto/AddressDto.cs
@@ -0,0 +1,37 @@
+﻿using EuroJobsCrm.Models;
+
+namespace EuroJobsCrm.Dto
+{
+    public class AddressDto
+    {
+        public int Id { get; set; }
+        public int? ClientId { get; set; }
+        public int? EmploeeId { get; set; }
+        public int? ContragentId { get; set; }
+        public string Type { get; set; }
+        public string Pay { get; set; }
+        public string PostCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Address { get; set; }
+
+        public AddressDto()
+        {
+            
+        }
+
+        public AddressDto(Addresses address)
+        {
+            Id = address.AdrId;
+            Address = address.AdrAddress;
+            City = address.AdrCity;
+            ClientId = address.ArdCltId;
+            ContragentId = address.AdrCgtId;
+            Country = address.AdrCountry;
+            EmploeeId = address.ArdEmpId;
+            Pay = address.AdrPay;
+            PostCode = address.AdrPostCode;
+            Type = address.AdrType;
+        }
+    }
+}
--- src/EuroJobsCrm/Models/DB_A12601_bielkaContext.cs
+++ src/EuroJobsCrm/Models/DB_A12601_bielkaContext.cs
@@ -14,6 +14,68 @@ namespace EuroJobsCrm.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Addresses>(entity =>
+            {
+                entity.HasKey(e => e.AdrId)
+                    .HasName("PK_Addresses");
+
+                entity.Property(e => e.AdrId).HasColumnName("adr_id");
+
+                entity.Property(e => e.AdrAddress)
+                    .HasColumnName("adr_address")
+                    .HasMaxLength(500);
+
+                entity.Property(e => e.AdrAduitMu)
+                    .HasColumnName("adr_aduit_mu")
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.AdrAduitRu)
+                    .HasColumnName("adr_aduit_ru")
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.AdrAuditCd)
+                    .HasColumnName("adr_audit_cd")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.AdrAuditCu)
+                    .HasColumnName("adr_audit_cu")
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.AdrAuditMd)
+                    .HasColumnName("adr_audit_md")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.AdrAuditRd)
+                    .HasColumnName("adr_audit_rd")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.AdrCgtId).HasColumnName("adr_cgt_id");
+
+                entity.Property(e => e.AdrCity)
+                    .HasColumnName("adr_city")
+                    .HasColumnType("varchar(100)");
+
+                entity.Property(e => e.AdrCountry)
+                    .HasColumnName("adr_country")
+                    .HasColumnType("varchar(100)");
+
+                entity.Property(e => e.AdrPay)
+                    .HasColumnName("adr_pay")
+                    .HasColumnType("varchar(1)");
+
+                entity.Property(e => e.AdrPostCode)
+                    .HasColumnName("adr_post_code")
+                    .HasColumnType("varchar(10)");
+
+                entity.Property(e => e.AdrType)
+                    .HasColumnName("adr_type")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.ArdCltId).HasColumnName("ard_clt_id");
+
+                entity.Property(e => e.ArdEmpId).HasColumnName("ard_emp_id");
+            });
+
             modelBuilder.Entity<AspNetRoleClaims>(entity =>
             {
                 entity.HasIndex(e => e.RoleId)
@@ -144,19 +206,25 @@ namespace EuroJobsCrm.Models
                     .HasColumnName("clt_audit_cd")
                     .HasColumnType("datetime");
 
-                entity.Property(e => e.CltAuditCu).HasColumnName("clt_audit_cu");
+                entity.Property(e => e.CltAuditCu)
+                    .HasColumnName("clt_audit_cu")
+                    .HasMaxLength(100);
 
                 entity.Property(e => e.CltAuditMd)
                     .HasColumnName("clt_audit_md")
                     .HasColumnType("datetime");
 
-                entity.Property(e => e.CltAuditMu).HasColumnName("clt_audit_mu");
+                entity.Property(e => e.CltAuditMu)
+                    .HasColumnName("clt_audit_mu")
+                    .HasMaxLength(100);
 
                 entity.Property(e => e.CltAuditRd)
                     .HasColumnName("clt_audit_rd")
                     .HasColumnType("datetime");
 
-                entity.Property(e => e.CltAuditRu).HasColumnName("clt_audit_ru");
+                entity.Property(e => e.CltAuditRu)
+                    .HasColumnName("clt_audit_ru")
+                    .HasMaxLength(100);
 
                 entity.Property(e => e.CltKrs)
                     .IsRequired()
@@ -179,6 +247,70 @@ namespace EuroJobsCrm.Models
                     .HasMaxLength(20);
             });
 
+            modelBuilder.Entity<ContactPersons>(entity =>
+            {
+                entity.HasKey(e => e.CtpId)
+                    .HasName("PK_ContactPersons");
+
+                entity.Property(e => e.CtpId).HasColumnName("ctp_id");
+
+                entity.Property(e => e.CtpCd)
+                    .HasColumnName("ctp_cd")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.CtpCgtId).HasColumnName("ctp_cgt_id");
+
+                entity.Property(e => e.CtpCltId).HasColumnName("ctp_clt_id");
+
+                entity.Property(e => e.CtpCu)
+                    .HasColumnName("ctp_cu")
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.CtpEmail)
+                    .HasColumnName("ctp_email")
+                    .HasMaxLength(250);
+
+                entity.Property(e => e.CtpMd)
+                    .HasColumnName("ctp_md")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.CtpMessanger)
+                    .HasColumnName("ctp_messanger")
+                    .HasMaxLength(150);
+
+                entity.Property(e => e.CtpMu)
+                    .HasColumnName("ctp_mu")
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.CtpName)
+                    .HasColumnName("ctp_name")
+                    .HasMaxLength(150);
+
+                entity.Property(e => e.CtpPhoneNumber)
+                    .HasColumnName("ctp_phone_number")
+                    .HasMaxLength(150);
+
+                entity.Property(e => e.CtpPosition)
+                    .HasColumnName("ctp_position")
+                    .HasMaxLength(250);
+
+                entity.Property(e => e.CtpRd)
+                    .HasColumnName("ctp_rd")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.CtpRu)
+                    .HasColumnName("ctp_ru")
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.CtpSkype)
+                    .HasColumnName("ctp_skype")
+                    .HasMaxLength(150);
+
+                entity.Property(e => e.CtpSurname)
+                    .HasColumnName("ctp_surname")
+                    .HasMaxLength(150);
+            });
+
             modelBuilder.Entity<Contragents>(entity =>
             {
                 entity.HasKey(e => e.CgtId)
@@ -190,7 +322,9 @@ namespace EuroJobsCrm.Models
                     .HasColumnName("cgt_audit_cd")
                     .HasColumnType("datetime");
 
-                entity.Property(e => e.CgtAuditCu).HasColumnName("cgt_audit_cu");
+                entity.Property(e => e.CgtAuditCu)
+                    .HasColumnName("cgt_audit_cu")
+                    .HasMaxLength(100);
 
                 entity.Property(e => e.CgtAuditMd)
                     .HasColumnName("cgt_audit_md")
@@ -198,7 +332,7 @@ namespace EuroJobsCrm.Models
 
                 entity.Property(e => e.CgtAuditMu)
                     .HasColumnName("cgt_audit_mu")
-                    .HasColumnType("datetime");
+                    .HasMaxLength(100);
 
                 entity.Property(e => e.CgtAuditRd)
                     .HasColumnName("cgt_audit_rd")
@@ -206,7 +340,7 @@ namespace EuroJobsCrm.Models
 
                 entity.Property(e => e.CgtAuditRu)
                     .HasColumnName("cgt_audit_ru")
-                    .HasColumnType("datetime");
+                    .HasMaxLength(100);
 
                 entity.Property(e => e.CgtLicenseNumber)
                     .IsRequired()
@@ -235,19 +369,25 @@ namespace EuroJobsCrm.Models
                     .HasColumnName("emp_audit_cd")
                     .HasColumnType("datetime");
 
-                entity.Property(e => e.EmpAuditCu).HasColumnName("emp_audit_cu");
+                entity.Property(e => e.EmpAuditCu)
+                    .HasColumnName("emp_audit_cu")
+                    .HasMaxLength(100);
 
                 entity.Property(e => e.EmpAuditMd)
                     .HasColumnName("emp_audit_md")
                     .HasColumnType("datetime");
 
-                entity.Property(e => e.EmpAuditMu).HasColumnName("emp_audit_mu");
+                entity.Property(e => e.EmpAuditMu)
+                    .HasColumnName("emp_audit_mu")
+                    .HasMaxLength(100);
 
                 entity.Property(e => e.EmpAuditRd)
                     .HasColumnName("emp_audit_rd")
                     .HasColumnType("datetime");
 
-                entity.Property(e => e.EmpAuditRu).HasColumnName("emp_audit_ru");
+                entity.Property(e => e.EmpAuditRu)
+                    .HasColumnName("emp_audit_ru")
+                    .HasMaxLength(100);
 
                 entity.Property(e => e.EmpBirthDate)
                     .HasColumnName("emp_birth_date")
@@ -270,6 +410,7 @@ namespace EuroJobsCrm.Models
             });
         }
 
+        public virtual DbSet<Addresses> Addresses { get; set; }
         public virtual DbSet<AspNetRoleClaims> AspNetRoleClaims { get; set; }
         public virtual DbSet<AspNetRoles> AspNetRoles { get; set; }
         public virtual DbSet<AspNetUserClaims> AspNetUserClaims { get; set; }
@@ -278,9 +419,8 @@ namespace EuroJobsCrm.Models
         public virtual DbSet<AspNetUserTokens> AspNetUserTokens { get; set; }
         public virtual DbSet<AspNetUsers> AspNetUsers { get; set; }
         public virtual DbSet<Clients> Clients { get; set; }
+        public virtual DbSet<ContactPersons> ContactPersons { get; set; }
         public virtual DbSet<Contragents> Contragents { get; set; }
         public virtual DbSet<Employees> Employees { get; set; }
-
-        // Unable to generate entity type for table 'dbo.Addresses'. Please see the warning messages.
     }
 }
