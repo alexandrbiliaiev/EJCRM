da1ce7d25e6f2dac5f36c9af97ec94bedb9934b8
Ivan Syrotiuk <ivan.syrotiuk@gmail.com>
2016-11-20 14:11:46 +0100

    sukiiiii

--- src/EuroJobsCrm/Dto/OfferDto.cs
+++ src/EuroJobsCrm/Dto/OfferDto.cs
@@ -51,19 +51,19 @@ namespace EuroJobsCrm.Dto
 
         public int Id { get; set; }
         public int ClientId { get; set; }
-        public int Branch { get; set; }
-        public int VacanciesNumber { get; set; }
-        public DateTime StartingDate { get; set; }
-        public DateTime EndingDate { get; set; }
+        public int? Branch { get; set; }
+        public int? VacanciesNumber { get; set; }
+        public DateTime? StartingDate { get; set; }
+        public DateTime? EndingDate { get; set; }
         public string WorkPlace { get; set; }
         public string Responsibilities { get; set; }
-        public int Education { get; set; }
+        public int? Education { get; set; }
         public string Experience { get; set; }
         public int? AgeFrom { get; set; }
         public int? AgeTo { get; set; }
         public string Languages { get; set; }
         public string Comments { get; set; }
-        public int ContractType { get; set; }
+        public int? ContractType { get; set; }
         public DateTime? WorkStart { get; set; }
         public DateTime? WorkEnd { get; set; }
         public double? OvertimeRate { get; set; }
@@ -71,20 +71,20 @@ namespace EuroJobsCrm.Dto
         public int WorkDays { get; set; }
         public double? RatePerHour { get; set; }
         public double? RatePerMonth { get; set; }
-        public int PaymentMethod { get; set; }
+        public int? PaymentMethod { get; set; }
         public double? AdvanceAmount { get; set; }
-        public int AccomodationType { get; set; }
-        public double AccomodationPrice { get; set; }
-        public int RoomPeopleNumber { get; set; }
-        public double DistanceToWork { get; set; }
+        public int? AccomodationType { get; set; }
+        public double? AccomodationPrice { get; set; }
+        public int? RoomPeopleNumber { get; set; }
+        public double? DistanceToWork { get; set; }
         public int? TransportToWork { get; set; }
-        public double TransportPrice { get; set; }
+        public double? TransportPrice { get; set; }
         public string Facilities { get; set; }
         public string AdditionalInfo { get; set; }
         public string Documents { get; set; }
         public string Position { get; set; }
 
-        public int Gender { get; set; }
+        public int? Gender { get; set; }
 
         public bool WorkMo
         {
