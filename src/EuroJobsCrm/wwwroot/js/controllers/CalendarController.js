6a291285d13ca12546dc16b207af30d5b7b86105
Ivan Syrotiuk <ivan.syrotiuk@gmail.com>
2017-01-09 16:18:06 +0100

    added calendar

--- src/EuroJobsCrm/wwwroot/js/controllers/CalendarController.js
+++ src/EuroJobsCrm/wwwroot/js/controllers/CalendarController.js
@@ -2,7 +2,7 @@ angular.module('EuroJobsCrm.controllers').controller('CalendarController',
     function ($scope, $location, $translate, $http, $state, $mdDialog, $routeParams, $cookies,
         calendarService) {
 
-        $scope.cal = 1;
 
+ 
 
     });
